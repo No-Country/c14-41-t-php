1b99dd07b08ecc65ea509a9bc35206075492514a
Damian-77 <damian_cano_77@hotmail.com>
2023-10-15 18:39:32 -0300

    ruta, controlador y modelo categorias, falta codigo

---
+++ api/take-food-backend/app/Http/Controllers/CategoriesController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Categories;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class CategoriesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Categories $categories)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Categories $categories)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Categories $categories)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Categories $categories)
+    {
+        //
+    }
+
+    // private static $rules = [
+    //     'id_categories' => 'required',
+    //     'description' => 'required'
+    // ];
+    
+    // private static $message = [
+    // ];
+
+    public function categories(){
+
+        // $data['categories'] = Categories::all();
+        // return with($data);
+
+        echo "Hola mundo";
+    }
+
+    // public function categories_edit(Request $request, $id){
+
+    //     $data = Categories::findOrFail($id);
+
+    //     $this->validate($request, static::$rules, static::$message);
+
+    //     //convierte el request en un array
+    //     $dataInfo = request()->except('_token');
+
+    //     $data->update($dataInfo);
+
+    //     return redirect('admin')->with("menssage", "Cambios Realizados");
+    // }
+}
