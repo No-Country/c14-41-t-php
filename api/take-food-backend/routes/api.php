68b4a32987ca80a9028849fc0dcd2ddcfc88d6c3
RuisuX10 <r.luisescobar@hotmail.com>
2023-10-16 21:35:41 -0300

    Agregado endpoint para editar datos de usuarios

--- api/take-food-backend/routes/api.php
+++ api/take-food-backend/routes/api.php
@@ -6,13 +6,16 @@ use App\Http\Controllers\CategoriesController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::resource('/user', UserController::class);
-//Route::resource('/user', UserController::class);
+//Ruta para crear usuario
 Route::post('/user/create', [UserController::class, 'createUser']);
+//Ruta para logear usuario
 Route::post('/user/login', [UserController::class, 'loginUser']);
+//Ruta para acceder a la
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+//Ruta para editar Usuario
+Route::middleware('auth:sanctum')->put('/user/edit', [UserController::class, 'editUser']);
 Route::get('/restaurant/info', [RestaurantController::class, 'general_info']);
 Route::put('/restaurant/info/{id}', [RestaurantController::class, 'general_info_edit']);
 Route::get('/categories', [CategoriesController::class, 'categories']);
