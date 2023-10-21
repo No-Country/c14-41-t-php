f314da504719f9be85f96224d979969a599afd25
Damian-77 <damian_cano_77@hotmail.com>
2023-10-21 16:14:05 -0300

    que se yo

--- api/take-food-backend/routes/api.php
+++ api/take-food-backend/routes/api.php
@@ -2,7 +2,6 @@
 
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\ProductController;
-use App\Http\Controllers\RestaurantController;
 use App\Http\Controllers\CategoriesController;
 use App\Http\Controllers\RestaurantController;
 use App\Http\Controllers\OrderController;
@@ -23,8 +22,6 @@ Route::get('/restaurant/info', [RestaurantController::class, 'general_info']);
 Route::put('/restaurant/info/{id}', [RestaurantController::class, 'general_info_edit']);
 Route::get('/categories', [CategoriesController::class, 'categories']);
 Route::put('/categories/{id}', [CategoriesController::class, 'categories_edit']);
-Route::post('/categories/create', [CategoriesController::class, 'create']);
 Route::get('/products', [ProductController::class, 'index']);
 
 Route::post('/crear-orden', [OrderController::class, 'crearOrden']);
-
