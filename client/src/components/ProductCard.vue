b5b8dccb2dcf335cc9dad85812158516345fd411
Leandro Insfran <leandro.insfran@systelec.com>
2023-11-01 21:38:15 -0300

    imge product

--- client/src/components/ProductCard.vue
+++ client/src/components/ProductCard.vue
@@ -1,14 +1,15 @@
 <template>
   <div class="card custom-card">
-     <img :src= "props.inmage" class="card-img-top custom-img" alt="comida"> 
-    <!-- <img src="@/assets/products/empanada1.jpg"
-      class="card-img-top custom-img" alt="comida">-->
+      <img v-if="props.id_category=='EMPANADAS'" :src= "empanadas" class="card-img-top custom-img" alt="comida"> 
+      <img v-if="props.id_category=='PIZZAS'" :src= "Pizzas" class="card-img-top custom-img" alt="comida"> 
+      <img v-if="props.id_category=='HAMBURGUESAS'" :src= "hamburguesas" class="card-img-top custom-img" alt="comida"> 
+      <img v-if="props.id_category=='PASTAS'" :src= "pastas" class="card-img-top custom-img" alt="comida"> 
+      <img v-if="props.id_category=='SANGUCHES'" :src= "sandwiches" class="card-img-top custom-img" alt="comida"> 
     <div class="card-body">
       <h5 class="card-title">{{ props.name }}</h5>
       <p class="card-text">{{ props.description }}</p>
       <p class="card-text">{{ props.cooking_time }} min para retirar </p>
       <b>$ {{ props.price }}</b>
-      {{props.image}} 
     </div>
     <button @click.prevent="AddCarrito(props.id)" class="btn btn-secondary">Agregar al carrito</button>
   </div>
@@ -19,7 +20,14 @@
 import { defineProps } from 'vue'
 import useCarrito from '@/store/carrito'
 
+import empanadas from '@/assets/products/empanada.jpg'
+import Pizzas from '@/assets/products/pizza.jpg'
+import hamburguesas from '@/assets/products/hamburguesa.jpg'
+import pastas from '@/assets/products/pastas.jpg'
+import sandwiches from '@/assets/products/sandwiches.jpg'
+
 const storeCarrito = useCarrito()
+
 
 const props = defineProps(
   {
@@ -36,6 +44,7 @@ const props = defineProps(
 const AddCarrito = (itemSelected) => {
   storeCarrito.producto.push(itemSelected)
   console.log('seleccion de carrito', storeCarrito.producto)
+  console.log(props)
 }
 
 </script>
